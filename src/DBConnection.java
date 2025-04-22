b52ff7c8509415b248c3c6dd03ed59b6dd216c3e
Harinandan M V <harinandanmv11@gmail.com>
2025-04-22 10:55:46 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,9 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String db_url = "jdbc:oracle:thin:@localhost:1521:orcl"; 
-    private static final String db_user = "system"; 
-    private static final String db_password = "Hari123#"; 
+    db_url=jdbc:oracle:thin:@localhost:1521:orcl
+    db_user=system
+    db_password=your_password_here
+ 
     private static Connection connection = null;
     
     public static Connection getConnection() throws SQLException {
